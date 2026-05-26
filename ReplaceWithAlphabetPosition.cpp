8eaa648c0c6dbb699e434ce0c38ebcbafb1c53c4
SrAxado <sr.axado@gmail.com>
2026-05-26 01:09:19 +0100

    Uploading the two other katas and .gitignore

--- ReplaceWithAlphabetPosition.cpp
+++ ReplaceWithAlphabetPosition.cpp
@@ -1,102 +1,32 @@
-// REPLACE WITH ALPHABET POSITION
-// https://www.codewars.com/kata/replace-with-alphabet-position/train/cpp
-
 #include <iostream>
 #include <string>
-#include <vector>
 
-
-// Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements 
-// with the same value next to each other and preserving the original element's order.
-
-template <typename T> std::vector<T> uniqueInOrder(const std::vector<T>& iterable){
-  std::vector<T> finalList;
+std::string alphabet_position(const std::string &text) {
+  std::string position_text = "";
   
-  if (iterable.size() > 0) {
-    for (int i = 0; i < iterable.size(); ++i) {  // Loop over the iterable string
-      int j = i + 1;  // Get the next element's index
-      while ((iterable[j] == iterable[i]) && (j < iterable.size())) {  // Loop until the next element is different from the current one
-        j++;
-      }
-      finalList.push_back(iterable[i]);  // Add the current element to the final list
-      i = j - 1;  // Points i to the last known duplicate (so that the next loop iteration will point to the next different element)
+  for (auto c : text) {
+    // We'll only consider the letters, discarding the numbers and the ponctuation
+    if (isalpha(c)) {
+      // Converting all letters to lowercase, we'll consider the letter 'a' as the baseline value 1
+      position_text += std::to_string(int(std::tolower(c)) - int('a') + 1) + ' ';
+//      std::cout << position_text << std::endl;
     }
   }
-  return finalList;
-}
-
-std::vector<char> uniqueInOrder(const std::string& iterable){
-  std::	vector<char> finalList; // This will be the list to be shared, filled and returned in the end
-  
-  if (iterable.size() > 0) {
-    for (int i = 0; i < iterable.size(); ++i) {  // Loop over the iterable string
-      int j = i + 1;  // Get the next element's index
-      while ((iterable[j] == iterable[i]) && (j < iterable.size())) {  // Loop until the next element is different from the current one
-        j++;
-      }
-      finalList.push_back(iterable[i]);  // Add the current element to the final list
-      i = j - 1;  // Points i to the last known duplicate (so that the next loop iteration will point to the next different element)
-    } 
-  }
-  return finalList;
-}
-
-
-/*
-// Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements 
-// with the same value, presenving the original element's order.
-
-template <typename T> std::vector<T> uniqueInOrder(const std::vector<T>& iterable){
-  std::vector<T> finalList; // This will be the list to be shared, filled and returned in the end
-  
-  if (iterable.size() > 0) {
-    for (auto it : iterable) {  // Loop over the iterable string
-      if (finalList.size() > 0) { // Checks if the it is already in the finalList
-        if (std::find(finalList.begin(), finalList.end(), it) == finalList.end()) {  // It doesn't exist in the finalList
-          finalList.push_back(it);
-          // std::cout << it << std::endl;
-        }
-      } else {  // The finalist is empty, so we can add the first element without checking
-        finalList.push_back(it);
-        // std::cout << it << std::endl;
-      }
-    }
+  // If a new string was created, then we'll have to remove the last character as it is a space
+  if (position_text.size() > 0) { // All of the following options are valid solutions
+    position_text.pop_back();
+//    position_text.resize(position_text.size() - 1);
+//  return position_text.substr(0, position_text.size() - 1);
   }
   
-  return finalList;
+  return position_text;
 }
 
-std::vector<char> uniqueInOrder(const std::string& iterable){
-  std::	vector<char> finalList; // This will be the list to be shared, filled and returned in the end
-  
-  if (iterable.size() > 0) {
-    for (auto it : iterable) {  // Loop over the iterable string
-      if (finalList.size() > 0) { // Checks if the it is already in the finalList
-        if (std::find(finalList.begin(), finalList.end(), it) == finalList.end()) {  // It doesn't exist in the finalList
-          finalList.push_back(it);
-          // std::cout << it << std::endl;
-        }
-      } else {  // The finalist is empty, so we can add the first element without checking
-        finalList.push_back(it);
-        // std::cout << it << std::endl;
-      }
-    } 
-  }
-  
-  return finalList;
-}
-
-*/
 
 int main() {
-  std::string testString = "AAAABBBCCDAABBB";
-  std::vector<char> result = uniqueInOrder(testString);
-  std::vector<int> testVector = {1, 2, 2, 3, 3};
-  std::vector<int> result2 = uniqueInOrder(testVector);
-  
-  for (auto it : result) {
-    std::cout << it << std::endl;
-  }
+  std::string text = "The sunset sets at twelve o' clock.";
+  std::cout << "Returned:\n" << alphabet_position(text) << std::endl;  
+  std::cout << "Should return: \n20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" << std::endl;
   
   return 0;
 }
