47d831a6bcfe7e5e6e404e148624b1dce53b11fa
SrAxado <sr.axado@gmail.com>
2026-06-03 00:39:35 +0100

    Tinkering spinWords2() from StopgninnipSMysdroW

--- StopgninnipSMysdroW.cpp
+++ StopgninnipSMysdroW.cpp
@@ -19,14 +19,14 @@ std::string spinWords(const std::string &str)
 
     if (endWord - beginWord >= 5) {  // Word with five or more letters
       word = result.substr(beginWord, (endWord - beginWord));
-      std::cout << word << std::endl;        
+      // std::cout << word << std::endl;        
       
       std::string reversedWord = "";
       for (std::string::reverse_iterator rit = word.rbegin(); rit != word.rend(); ++rit) {
         reversedWord += *rit;
       }
   
-      std::cout << reversedWord << std::endl;
+      // std::cout << reversedWord << std::endl;
       result.replace(beginWord, reversedWord.size(), reversedWord);
     }
 
@@ -38,10 +38,11 @@ std::string spinWords(const std::string &str)
 
         do {
           ++endWord;
-        } while (result[endWord + 1] == ' ');
+        } while (result[endWord] == ' ');
         
-        if (endWord == std::string::npos) {  // The last characters of the text are spaces -- we need to delete them
+        if (endWord == result.size()) {  // The last characters of the text are spaces -- we need to delete them
           result.erase(beginWord, std::string::npos);
+          break;  // We have reached the end of the text, so we can break the loop
         }
       }
       
@@ -55,10 +56,65 @@ std::string spinWords(const std::string &str)
 }
 
 
+std::string spinWords2(const std::string &str) {
+  std::string result = "";
+  size_t beginWord = 0, endWord = 0;
+
+  for (size_t i = 0; i < str.size(); ++i) {
+    endWord = str.find(' ', i);
+
+    if (endWord - i >= 5) {
+      for (size_t rit = endWord - 1; rit >= i; --rit) {
+        result += str[rit];
+      }
+
+      result += str[endWord]; // Adds the space after the reversed word
+
+      // beginWord += endWord - beginWord; // Advances beginWord pointer the word's length
+      // i = endWord + 1;  // Advances beginWord pointer the word's length
+    
+    } else {
+      for (size_t it = i; it <= endWord; ++it) {
+        result += str[it];
+      }
+    }
+    
+    i = endWord;  // Advances i to point to the next word
+
+    if (str[endWord + 1] == ' ') {
+      size_t lastSpace = endWord;
+      do {  // Searches for a possible spaces end trail and gets to its end
+        ++lastSpace;
+      } while (str[lastSpace] == ' ');
+
+      if (lastSpace == str.size()) { // It's a spaces end trail -- not to add it to the result
+        break;
+      
+      } else {  // If it's not a spaces end trail, it should add the consecutive spaces to the result
+        for (size_t it = i; it < lastSpace; ++it) { // Copies str from i to the lastSpace space character
+          result += str[it];
+        }
+        // result += std::string(' ', lastSpace - endWord);
+        i = lastSpace;  //  Points i to the lastSpace character
+        continue;
+      }
+    }
+
+    if (endWord == std::string::npos) { // Last word with non space character in the end
+      endWord = str.size();
+    }    
+  }
+
+  return result;
+}
+
+
 int main() {
 
-  std::string spinnedWordsText = spinWords("Hey fellow warriors");
-  spinnedWordsText = spinWords("This is a test    ");
+  std::string spinnedWordsText;
+  // spinnedWordsText = spinWords("Hey fellow warriors");
+  spinnedWordsText = spinWords2("This is   a test    ");
+  // spinnedWordsText = spinWords2("This is another test");
 
   std::cout << "'" << spinnedWordsText << "'" << std::endl;
 
